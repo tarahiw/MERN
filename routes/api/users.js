417c322f8533c2e28a5e5a1c951786837ea54a98
Sam Mahdavian <tarahiw@yahoo.com>
2019-08-15 01:16:54 +0430

    register user and validation data az req.body and init middleware express json create model user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
-
-// @router    GET api/users
-// @desc      Test route
+const { check, validationResult } = require('express-validator');
+// @router    POST api/users
+// @desc      Register user
 // @access    public
-router.get('/', (req, res) => {
-  res.send('User router');
-});
+router.post(
+  '/',
+  [
+    check('name', 'پر کردن فیلد نام اجباری است')
+      .not()
+      .isEmpty(),
+    check('email', 'لطفا یک ایمیل معتبر وارد کنید').isEmail(),
+    check('password', 'پسور باید بیش از ۶ کاراکتر داشته باشد').isLength({
+      min: 6
+    })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('User router');
+  }
+);
 
 module.exports = router;
