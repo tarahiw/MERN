6c501ecc27fbf9c4bdc1fe920636ef7d539b3099
Sam Mahdavian <tarahiw@yahoo.com>
2019-08-15 15:10:07 +0430

    User Registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
+const User = require('../../Models/User');
+
 // @router    POST api/users
 // @desc      Register user
 // @access    public
@@ -15,12 +19,45 @@ router.post(
       min: 6
     })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('User router');
+
+    const { name, email, password } = req.body;
+
+    try {
+      // see if user exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'این یوزر قبلا موجود بوده' }] });
+      }
+      // get users gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+      // encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+      // return jsonwebtoken
+      res.send('یوزر رجیستر شد');
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('server error');
+    }
   }
 );
 
