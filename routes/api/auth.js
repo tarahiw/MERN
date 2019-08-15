874bb05f332b14e6b22c54ae036dd48c2649180d
Sam Mahdavian <tarahiw@yahoo.com>
2019-08-15 17:40:26 +0430

    6. User Authentication  Login Route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+
 const User = require('../../Models/User');
+
 // @router    GET api/auth
 // @desc      Test route
 // @access    public
@@ -15,5 +21,60 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('server error');
   }
 });
+
+// @router    POST api/auth
+// @desc      authenticate user & get token
+// @access    public
+router.post(
+  '/',
+  [
+    check('email', 'لطفا یک ایمیل معتبر وارد کنید').isEmail(),
+    check('password', 'پرورد اجباری است').exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      // see if user exists
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ errors: [{ msg: 'ایمیل نامعتبر است' }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'پسورد نا معتبر است' }] });
+      }
+
+      // return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('server error');
+    }
+  }
+);
 
 module.exports = router;
